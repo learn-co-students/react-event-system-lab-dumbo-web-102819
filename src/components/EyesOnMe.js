84c3d0a1e788db2918439e13b2abefbdbee4aeec
Dominic Chu <dominic.m.chu@gmail.com>
2020-01-07 10:35:47 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+    render () {
+        return (
+            <button onFocus={this.handleMouseIn} onBlur={this.handleMouseOut}/>
+        )
+    }
+    handleMouseOut = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    handleMouseIn = () => {
+        console.log('Good!')
+    }
+}
